301dc75b5fb24535e90d51522ba1169c2188d19f
Randy Kipkurui <randy.kipkurui4@gmail.com>
2023-06-05 09:19:00 +0300

    - Add a shoppingList component connected to React Devtools. - Use configureStore instead of createStore.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import counterReducer from "./features/counter/counterSlice.js";
+import { Provider } from "react-redux"
+// import { createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./features/ShoppingList/shoppingListSlice";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = configureStore({
+  reducer: counterReducer,
+
+},
+window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
